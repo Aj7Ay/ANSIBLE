a14d5cc287d576e7e107ab88b24c4a60efb3ea79
Aj7ay <ajayyegireddi007@gmail.com>
2023-10-30 22:06:14 +0530

    updated

--- Jenkins-Ec2/install_jenkins.sh
+++ Jenkins-Ec2/install_jenkins.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+exec > >(tee -i /var/log/user-data.log)
+exec 2>&1
 sudo apt update -y
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
+sudo apt install git -y 
+mkdir Ansible && cd Ansible
+pwd
+git clone https://github.com/Aj7Ay/ANSIBLE.git
+cd ANSIBLE
+ansible-playbook -i localhost Jenkins.yml
